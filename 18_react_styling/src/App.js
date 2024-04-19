d64c27399d6cf7f08608588d549b1c497469039f
anu <anupurew1224@gmail.com>
2024-04-19 16:58:03 +0900

    study: react router, styling component, sass

---
+++ 18_react_styling/src/App.js
@@ -0,0 +1,16 @@
+import BasicCss from "./components/BasicCss";
+import ModuleCss from "./components/ModuleCss";
+import StyledComponent from "./components/StyledComponent";
+import Sass from "./components/Sass";
+function App() {
+  return (
+    <div className="App">
+      <BasicCss></BasicCss>
+      <ModuleCss></ModuleCss>
+      <StyledComponent></StyledComponent>
+      <Sass></Sass>
+    </div>
+  );
+}
+
+export default App;
